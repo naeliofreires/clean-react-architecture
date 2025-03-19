6ca38fc6624810bce886ae58add510552b8c6dd6
Naélio Freires <naeliosistemas@gmail.com>
2025-03-19 17:32:13 -0300

    test: refactor Jest configuration to ES module syntax and enhance test scripts for better execution.

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,15 @@
-module.exports = {
-    clearMocks: true,
-    roots: ["<rootDir>/src"],
-    collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}"],
-    coverageDirectory: "coverage",
-    moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
-    testEnvironment: "node",
-    testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
-    transform: {
-        "^.+\\.tsx?$": "ts-jest",
-    },
-};
+export default {
+  clearMocks: true,
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  coverageDirectory: 'coverage',
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+  testEnvironment: 'node',
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+}
